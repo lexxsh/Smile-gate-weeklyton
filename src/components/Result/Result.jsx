a2247b37faf9f7c54a024072edd5e8b2eb2fe963
lexxsh <john8538@naver.com>
2024-07-27 04:34:14 +0900

    g

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -5,10 +5,24 @@ import { useNavigate } from 'react-router-dom'
 
 const Result = () => {
   const [bgColor, setBgColor] = useState('#ddcfb9') // 초기 배경색 설정
+  const [videoId] = useState('fqimqhjpLjU') // 비디오 ID 상태
   const navigate = useNavigate()
 
   const handleClick = () => {
     navigate('/result1')
+  }
+
+  const opts = {
+    height: '0', // 비디오가 보이지 않도록 설정
+    width: '0',
+    playerVars: {
+      autoplay: 1, // 자동 재생
+      controls: 0, // 컨트롤 숨기기
+      modestbranding: 1, // 브랜드 로고 숨기기
+      rel: 0, // 관련 비디오 숨기기
+      iv_load_policy: 3, // 비디오 광고 숨기기
+      cc_load_policy: 0, // 자막 숨기기
+    },
   }
 
   useEffect(() => {
@@ -35,25 +49,14 @@ const Result = () => {
   }, []) // 빈 배열로 설정하여 컴포넌트 마운트 시 한 번만 실행
 
   const onReady = event => {
-    // 비디오가 로드되면 음소거하고 자동 재생
-    event.target.mute()
+    // 비디오가 로드되면 자동 재생
     event.target.playVideo()
   }
 
-  const opts = {
-    height: '0', // 비디오가 보이지 않도록 설정
-    width: '0',
-    playerVars: {
-      autoplay: 1, // 자동으로 비디오 재생
-      controls: 0, // 컨트롤 숨기기
-      showinfo: 0, // 비디오 정보 숨기기
-      modestbranding: 1, // 브랜드 로고 숨기기
-      iv_load_policy: 3, // 비디오에 광고 표시하지 않기
-      cc_load_policy: 0, // 자막 숨기기
-      rel: 0, // 관련 비디오 숨기기
-      loop: 1, // 비디오 루프
-      playlist: '5qap5aO4i9A', // 루프용 비디오 ID
-    },
+  const onStateChange = event => {
+    // 상태 변화에 따라 동작 정의 가능
+    // 예: 상태를 추적하거나 추가적인 작업을 수행할 수 있음
+    console.log('Player state changed:', event.data)
   }
 
   return (
@@ -68,9 +71,10 @@ const Result = () => {
 
       <div className="Result-YouTubeContainer">
         <YouTube
-          videoId="5qap5aO4i9A" // 여기에 유튜브 비디오 ID 입력
+          videoId={videoId} // 비디오 ID 설정
           opts={opts}
           onReady={onReady}
+          onStateChange={onStateChange}
         />
         <div className="YouTube-Content">추천 음악</div>
       </div>
